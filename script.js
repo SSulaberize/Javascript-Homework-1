42b67d1df4d58c19b752eb46ec90745cb383544f
SSulaberidze <ssulberidze@gmail.com>
2022-09-20 23:21:48 +0400

    Create script.js
    სადაც ორი სხვადახვა გზით უნდა შესრულებულიყო დავალება ორმაგი კომენტარი მიდევს ზოგიერთ პასუხზე.
    ყოველ დავალებას შორის 3 ხაზია გამოტოვებული.

---
+++ script.js
@@ -0,0 +1,90 @@
+
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'smith',
+//     age: 25,
+//     studentstatus: 'active',
+// };
+// console.log(user.studentstatus);
+
+
+
+// let items = ['house', 'car', 15, 'melon', 'apple', 2, 'tree'];
+
+// // for (let y of items){
+// //     console.log(y); 
+// // }
+
+// let i=0;
+// while (i<items.length){
+//     console.log(items[i]);
+//     i++;
+// }
+
+
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// for (let a = 0;  a < numbers.length; a++){
+//     if (numbers [a] > 5){
+//         console.log(numbers[a]);
+//     }
+// }
+
+
+
+// let user = {
+//     name: 'giorgi',
+//     age: 20,
+//     studentstatus: 'active',
+// };
+
+// if (user.age < 18 && user.studentstatus == 'active'){
+//     console.log('Hello');
+// }
+// else if (user.name == 'levani'){
+//     console.log('Hello Levani');
+// }
+// else if (user.studentstatus == 'active' || user.age < 25){
+//     console.log('Hello World');
+// }
+// else {
+//     console.log('Error');
+// }
+
+
+
+// let array = ['watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'];
+
+// console.log(array [0], array [1], array [array.length-2], array [array.length-1]);
+
+
+
+
+// let array = [[2, -3, 5, 10], [25, -24, -11, 100], [-6, -7, 10]];
+
+// console.log(array [0][0], array [0][2], array [0][3], array [1][0], array [1][3], array [2][2]);
+
+
+
+// let array = [ 2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10 ];
+
+// // for (let item of array){
+// //     if (item %2 == 0){
+// //         console.log(item);
+// //     }
+// // }
+
+// for (let item of array){
+//     if (item %2 == 1){
+//         console.log(item);
+//     }
+// }
+
+
+
+// let users = [
+//     {username: 'giorgi', status: false},
+//     {username: 'levani', status: false},
+//     {username: 'anna', status: true},
+// ];
+// console.log(users[2].username);
